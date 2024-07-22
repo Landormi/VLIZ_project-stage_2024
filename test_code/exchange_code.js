2c4532457705bdffe4cc46b5b84454730702f37d
Maxime Albert <102310804+Landormi@users.noreply.github.com>
2024-07-22 13:13:16 +0000

    test netlif

---
+++ test_code/exchange_code.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch';
+
+export async function handler(event, context) {
+    const { code } = event.queryStringParameters;
+    const CLIENT_ID = 'Ov23lixU1XxqIib5KkT3';
+    const CLIENT_SECRET = '326909b94f082c5cda2d261f0decda36e671de4e';
+
+    const tokenResponse = await fetch('https://github.com/login/oauth/access_token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET,
+            code: code
+        })
+    });
+
+    const tokenData = await tokenResponse.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(tokenData)
+    };
+}

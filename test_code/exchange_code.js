564bad68db55271bd0dbcd48cc95c3aaa657304d
Maxime Albert <102310804+Landormi@users.noreply.github.com>
2024-07-23 08:09:24 +0000

    ...

--- test_code/exchange_code.js
+++ test_code/exchange_code.js
@@ -1,6 +1,6 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
-export async function handler(event, context) {
+exports.handler = async function(event, context) {
     const { code } = event.queryStringParameters;
     const CLIENT_ID = 'Ov23lixU1XxqIib5KkT3';
     const CLIENT_SECRET = '326909b94f082c5cda2d261f0decda36e671de4e';
@@ -24,4 +24,4 @@ export async function handler(event, context) {
         statusCode: 200,
         body: JSON.stringify(tokenData)
     };
-}
+};

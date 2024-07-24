4597efd58e0e90460d94ac2910f09bdca49e4d38
Maxime Albert <102310804+Landormi@users.noreply.github.com>
2024-07-24 11:27:37 +0200

    Update exchange_code.js

--- test_code/exchange_code.js
+++ test_code/exchange_code.js
@@ -2,8 +2,7 @@ const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
     const { code } = event.queryStringParameters;
-    const CLIENT_ID = 'Ov23lixU1XxqIib5KkT3';
-    const CLIENT_SECRET = '326909b94f082c5cda2d261f0decda36e671de4e';
+    
 
     const tokenResponse = await fetch('https://github.com/login/oauth/access_token', {
         method: 'POST',
